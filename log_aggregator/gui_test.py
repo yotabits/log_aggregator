3c17a9aa965ef35ab828fb03e6ecb194e6a8601e
Thomas <thomas.kostas@yujinrobot.com>
2017-12-20 16:18:57 +0900

    Added problem description feature
    Now it's possible to fill a text area to describe the problem encountered
    at the time the tarball was created. If this text area is filled a bug
    description file will be be part of the tarball created.

--- log_aggregator/gui_test.py
+++ log_aggregator/gui_test.py
@@ -1,7 +1,5 @@
 from appJar import gui
-from cStringIO import StringIO
-import colors
-import sys
+import os
 import time
 import thread
 from opt_parser import manage_local, manage_conf_update, manage_show_conf
@@ -23,10 +21,13 @@ class Gui:
             manage_show_conf(None, no_check=True)
             self.app.infoBox("Take a look Closer", "Configuration file status displayed in console.")
         if (button == "Tar Me"):
+            description_file_path = self.get_description()
             self.app.showSubWindow("sub1")
-            manage_local(None, no_check=True)
+            manage_local(None, no_check=True, description_file=description_file_path)
             self.app.hideSubWindow("sub1")
             self.app.infoBox("success", "Success Fully created Tarball, Read console log for more details")
+            self.app.clearTextArea("pb_describe")
+            self.app.setTextArea("pb_describe", "Describe your problem")
 
     def create_progress_subwindow(self):
         self.app.startSubWindow("sub1", title="Pleas Wait...", modal=False, transient=False,)
@@ -45,7 +46,20 @@ class Gui:
         thread.start_new_thread(self.loop_progress, ())
 
     def configure_main_window(self):
+        self.app.addTextArea("pb_describe")
+        self.app.setTextArea("pb_describe", "Describe your problem")
         self.app.addButtons(["Show configuration", "Tar Me"], self.button_manager_main)
         self.app.addFileEntry("file_path")
         self.app.addButton("Validate file", self.button_manager_main)
 
+    def get_description(self):
+        input = self.app.getTextArea("pb_describe")
+        if(input != "Describe your problem"):#here we should save in file
+            description_file_name = "bug describer"
+            fp = open(description_file_name, "w")
+            fp.write(input)
+            fp.close()
+            cwd = os.getcwd()
+            file_path = cwd + "/" + description_file_name
+            return file_path
+        return None
