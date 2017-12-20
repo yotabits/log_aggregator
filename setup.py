ddbc03f18d1a1379ae54414b9bebfe348735abcf
Thomas Kostas <thomas.kostas@yujinrobot.com>
2017-12-20 13:32:03 +0900

    Update setup.py for release
    -Added classifier informations
    -Added repository url

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='log_aggregator',
       version='0.1',
       description='Create a configurable Tarball of various files in the file system',
       author='Thomas Kostas',
+      url='https://github.com/yotabits/log_aggregator'
       author_email='tkostas75@gmail.com',
       license='MIT',
       packages=['log_aggregator'],
@@ -11,4 +12,11 @@ setup(name='log_aggregator',
       install_requires=['pycurl',
                         'appjar'
       ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        ],
       zip_safe=False)
