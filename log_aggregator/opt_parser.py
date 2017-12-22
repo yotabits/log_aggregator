761fb2b09fcae492fc318bc18c15b714b28a0818
Thomas <thomas.kostas@yujinrobot.com>
2017-12-22 14:25:02 +0900

    Added command line option
    Now --version/-v can be used from command line

--- log_aggregator/opt_parser.py
+++ log_aggregator/opt_parser.py
@@ -4,7 +4,7 @@ import tar_manager
 
 def pars_args():
     parser = argparse.ArgumentParser(description="A simple, configurable software to tar a set of preconfigured log "
-                                                 "files.")
+                                                 "files.", prog='log_aggregator', version='0.22')
     parser.add_argument("--show_conf", action="store_true", default=False, help="Show the actual configuration file in"
                                                                                 " use if it exists")
 
