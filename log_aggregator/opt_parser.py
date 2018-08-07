54e1ea3dd62e01ba3928258443f990c406898d8d
Thomas <thomas.kostas@yujinrobot.com>
2018-08-07 11:06:01 +0900

    Correctly update version
    Version is now correctly set in setup and code.

--- log_aggregator/opt_parser.py
+++ log_aggregator/opt_parser.py
@@ -4,7 +4,7 @@ import tar_manager
 
 def pars_args():
     parser = argparse.ArgumentParser(description="A simple, configurable software to tar a set of preconfigured log "
-                                                 "files.", prog='log_aggregator', version='0.41')
+                                                 "files.", prog='log_aggregator', version='0.43')
     parser.add_argument("--show_conf", action="store_true", default=False, help="Show the actual configuration file in"
                                                                                 " use if it exists")
 
